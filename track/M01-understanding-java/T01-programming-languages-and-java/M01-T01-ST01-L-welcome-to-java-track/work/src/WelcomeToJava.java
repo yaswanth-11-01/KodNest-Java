33c2bf8ab87da4473701042b20294d05b559f8f6
yaswanth-11-01 <varikutiyaswanth950@gmail.com>
2026-07-30 13:01:16 +0530

    added one more line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,7 +1,10 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       System.out.println("Welcome to the Java track.");
-       System.out.println("I already know how to build programming logic.");
-       System.out.println("Now I am learning how to express that logic using Java.");
+        System.out.println("Welcome to the Java track.");
+        System.out.println("I am yaswanth.");
+        System.out.println("I already know how to build programming logic.");
+        System.out.println("Now I am learning how to express that logic using Java.");
     }
 }
