6a1fbe43e8b46bf4729f8268dd3649197c8dcbb7
yaswanth-11-01 <varikutiyaswanth950@gmail.com>
2026-08-03 17:51:18 +0530

    PO-3

---
+++ track/MOD2/TOPICS/PO-2/LearnerDetails.java
@@ -0,0 +1,11 @@
+
+public class LearnerDetails {
+
+    public static void main(String[] args) {
+        String learnerName = "Salman";
+        int dailyPracticeHours = 8;
+
+        System.out.println("Name: " + learnerName);
+        System.out.println("Daily Practice: " + dailyPracticeHours + " Hours");
+    }
+}
