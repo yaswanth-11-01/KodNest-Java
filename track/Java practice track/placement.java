c947eb05d62ed3d6030e8777c521205986f3f361
yaswanth-11-01 <varikutiyaswanth950@gmail.com>
2026-08-04 18:01:06 +0530

    placementeligibility

--- track/Java practice track/placement.java
+++ track/Java practice track/placement.java
@@ -3,14 +3,12 @@ public class placement {
 
     public static void main(String[] args) {
 
-        // Fixed values
         int marks = 72;
         int attendance = 80;
         boolean activeBacklog = false;
         boolean projectCompleted = true;
         int mockInterviewScore = 65;
 
-        // Check conditions in order
         if (marks < 60 || attendance < 75 || activeBacklog) {
             System.out.println("Improve Academic Eligibility");
         } else if (!projectCompleted) {
