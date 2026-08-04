907ba256026ae5769e30d7073b24b8d97cd3260e
yaswanth-11-01 <varikutiyaswanth950@gmail.com>
2026-08-04 11:37:02 +0530

    weeks

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
+    }
+}
