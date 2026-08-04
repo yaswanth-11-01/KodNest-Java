8f4324cd06fc02528460a3b9d818b19e3a04cd90
yaswanth-11-01 <varikutiyaswanth950@gmail.com>
2026-08-04 17:54:03 +0530

    pr-3

---
+++ track/Java practice track/placement.java
@@ -0,0 +1,24 @@
+
+public class placement {
+
+    public static void main(String[] args) {
+
+        // Fixed values
+        int marks = 72;
+        int attendance = 80;
+        boolean activeBacklog = false;
+        boolean projectCompleted = true;
+        int mockInterviewScore = 65;
+
+        // Check conditions in order
+        if (marks < 60 || attendance < 75 || activeBacklog) {
+            System.out.println("Improve Academic Eligibility");
+        } else if (!projectCompleted) {
+            System.out.println("Complete the Project");
+        } else if (mockInterviewScore < 70) {
+            System.out.println("Improve Interview Skills");
+        } else {
+            System.out.println("Placement Ready");
+        }
+    }
+}
