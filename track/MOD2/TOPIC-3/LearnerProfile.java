cecfe2843fa4eef6adbd1ffb63d166caf3da22b3
yaswanth-11-01 <varikutiyaswanth950@gmail.com>
2026-08-07 11:52:52 +0530

    LearnerProfile

---
+++ track/MOD2/TOPIC-3/LearnerProfile.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class LearnerProfile {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first name:");
+        String name = sc.next();
+        System.out.println("Learner: " + name);
+
+        System.out.println("Enter the solvedProblems:");
+        int num = sc.nextInt();
+        System.out.println("Problems solved: " + num);
+
+        System.out.println("Enter the assessmentPercentage:");
+        double d = sc.nextDouble();
+        System.out.println("Assessment: " + d);
+
+        sc.close();
+    }
+}
