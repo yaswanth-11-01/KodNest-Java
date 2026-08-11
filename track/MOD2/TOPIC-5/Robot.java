42acaffaca6ca5844b3ef805f4656ca139fa1642
yaswanth-11-01 <varikutiyaswanth950@gmail.com>
2026-08-11 17:51:25 +0530

    Topic-5

---
+++ track/MOD2/TOPIC-5/Robot.java
@@ -0,0 +1,18 @@
+
+public class Robot {
+
+    void speak() {
+        // Print the message
+        System.out.println("Beep beep! Java is my superpower!");
+    }
+
+    public static void main(String[] args) {
+
+        // Create a Robot object
+        Robot robot = new Robot();
+
+        // Call the speak() method
+        robot.speak();
+    }
+}
+
