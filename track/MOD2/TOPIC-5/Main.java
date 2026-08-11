9b98591d8ed7c8a78d8c9703be619264cee6001a
yaswanth-11-01 <varikutiyaswanth950@gmail.com>
2026-08-11 18:32:28 +0530

    Topic-5

---
+++ track/MOD2/TOPIC-5/Main.java
@@ -0,0 +1,59 @@
+
+import java.util.Scanner;
+
+class StudentUtility {
+
+    void showReportTitle() {
+        System.out.println("Student Performance Report");
+    }
+
+    void displayStudent(int id, String name) {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+    }
+
+    double getPassingPercentage() {
+        return 60.0;
+    }
+
+    double calculatePercentage(int javaScore, int sqlScore) {
+        return (javaScore + sqlScore) / 2.0;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        StudentUtility utility = new StudentUtility();
+
+        // Read input
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        String name = scanner.nextLine();
+
+        int javaScore = scanner.nextInt();
+        int sqlScore = scanner.nextInt();
+
+        // Call methods
+        utility.showReportTitle();
+        utility.displayStudent(id, name);
+
+        double percentage = utility.calculatePercentage(javaScore, sqlScore);
+        double passingPercentage = utility.getPassingPercentage();
+
+        System.out.println("Percentage: " + percentage);
+
+        // Final decision in main()
+        if (percentage >= passingPercentage) {
+            System.out.println("Result: PASS");
+        } else {
+            System.out.println("Result: NEEDS IMPROVEMENT");
+        }
+
+        scanner.close();
+    }
+}
