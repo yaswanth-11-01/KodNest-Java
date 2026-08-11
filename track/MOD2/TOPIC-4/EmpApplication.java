3df22f3f49fb687df8b20e6bb479506b443bd0ae
yaswanth-11-01 <varikutiyaswanth950@gmail.com>
2026-08-11 11:15:46 +0530

    EmpApplication

---
+++ track/MOD2/TOPIC-4/EmpApplication.java
@@ -0,0 +1,32 @@
+
+class Employee {
+
+    int id;
+    String name;
+
+    void work() {
+        System.out.println("Working");
+    }
+}
+
+class EmpApplication {
+
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee();
+        e1.id = 11;
+        e1.name = "yaswanth";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+        e1.work();
+
+        Employee e2;
+        e2 = e1;
+        e2.id = 13;
+        e2.name = "Anu";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+        System.out.println(e2.id);
+        System.out.println(e2.name);
+    }
+}
