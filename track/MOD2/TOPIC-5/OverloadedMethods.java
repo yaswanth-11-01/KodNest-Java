5d9160ab411a81bfc47aef05592c21bacb61dfc7
yaswanth-11-01 <varikutiyaswanth950@gmail.com>
2026-08-12 14:15:02 +0530

    Overloaded Methods

---
+++ track/MOD2/TOPIC-5/OverloadedMethods.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+class Printer {
+
+    void show(int number) {
+        System.out.println("Number: " + number);
+    }
+
+    void show(String word) {
+        System.out.println("Word: " + word);
+    }
+}
+
+public class OverloadedMethods {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        String word = scanner.next();
+
+        Printer printer = new Printer();
+
+        printer.show(number);
+        printer.show(word);
+    }
+}
