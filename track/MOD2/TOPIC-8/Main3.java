9a07dc0ce6cbc6084f93492f45e86a5a6c394eab
yaswanth-11-01 <varikutiyaswanth950@gmail.com>
2026-08-19 11:55:20 +0530

    TOPIC-8

---
+++ track/MOD2/TOPIC-8/Main3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+        int a[][] = new int[3][5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Array Elements :");
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 4; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Array Elements are :");
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 4; j++) {
+                    System.out.println(arr[i] + " ");
+                }
+                System.out.println();
+        }
+    }
+}
